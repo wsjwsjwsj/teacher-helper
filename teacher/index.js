7eda336dc5e73d3a4b269a86cff0d8d1ae09fcaa
wsjwsjwsj <632069405@qq.com>
2017-06-02 21:17:54 +0800

    third commit
    个人信息测试成功

--- teacher/index.js
+++ teacher/index.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var crypto = require('crypto');
 var session = require('express-session');
 var moment = require('moment');
-var cookieParser = require('cookie-parser');    //导入中间件
+var cookieParser = require('cookie-parser');    
 var request = require('request');
 var fs = require('fs');
 
@@ -28,47 +28,87 @@ app.use(session({
    saveUninitialized:true
 }));
 
-
-app.get('/',function(req,res){
-    if(req.session.user)     
-       return res.redirect('/user'); 
-    var code = req.query.code;
-        request('https://qyapi.weixin.qq.com/cgi-bin/user/getuserinfo?access_token='+getToken().access_token+'&code=' + code, function(err, ress, data){
-           if(!JSON.parse(data).errcode){
-             req.session.user = JSON.parse(data);       
-           return res.redirect('/user');  
-                 
-           }   
-           else{
-
-           }
-
-         }); 
-
-   });
-
-app.get('/user',function(req,res){
-       console.log(req.session.user);
-       res.render('student',{});
-   });
-
 function getToken(){
     var token;
     if(fs.existsSync('token.dat')){
-      token = JSON.parse(fs.readFileSync('token.dat'));
+        token = JSON.parse(fs.readFileSync('token.dat'));
     }
 
     if(!token || token.timeout < Date.now()){
-      request('https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=wx1d3765eb45497a18&corpsecret=9CFgKICkBIL5L1iKzzPd1XcThRkPc3lLeTa9Cm5hQ1q3Xm_YBL0rut-oMzvM4PN-', function(err, res, data){
-        var result = JSON.parse(data);
-        result.timeout = Date.now() + 7200;
-        fs.writeFileSync('token.dat', JSON.stringify(result));
-        token = result;
-      });      
+        request('https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=wx1d3765eb45497a18&corpsecret=9CFgKICkBIL5L1iKzzPd1XcThRkPc3lLeTa9Cm5hQ1q3Xm_YBL0rut-oMzvM4PN-', function(err, res, data){
+        	var result = JSON.parse(data);
+        	result.timeout = Date.now() + 7200;
+        	fs.writeFileSync('token.dat', JSON.stringify(result));
+        	token = result;
+        });      
     }
-   // console.log(token);
     return token;
 }
+
+function getTeacherInfo(teacherid, token, res){
+	process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+	request('https://api.mysspku.com/index.php/V2/TeacherInfo/getDetail?teacherid='+teacherid+'&token='+token, function(err, ress, data){
+		var teacher;
+		if(err)
+			console.log(err);
+        var result = JSON.parse(data);
+        console.log(result);
+        if(result.errcode == 0){
+        	teacher = result.data;
+        	res.render('teacherInfo',{
+        			title: "个人信息",
+        			teacher: teacher,
+        			img: '<img class="am-thumbnail" id="photo" name="photo" src="'+ teacher.imgurl +'" style="height:160px;width:100% auto;"></img>'
+        		}); 
+        }    
+    });  
+      
+}
+
+function getStudentInfo(){
+
+}
+
+app.get('/student',function(req,res){
+    if(req.session.user)     
+        res.render('student',{});
+    else{ 
+    	var code = req.query.code;
+        request('https://qyapi.weixin.qq.com/cgi-bin/user/getuserinfo?access_token='+getToken().access_token+'&code=' + code, function(err, ress, data){
+            if(!JSON.parse(data).errcode){
+                req.session.user = JSON.parse(data);       
+            res.render('student',{});  
+            }   
+           else{
+           }
+        });
+        
+        } 
+   });
+
+app.get('/teacher',function(req,res){
+    if(req.session.user){
+    	console.log(req.session.user);
+    	req.session.user.UserId = '12154545';
+    	getTeacherInfo(req.session.user.UserId, 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', res);
+    }
+    else{
+    	var code = req.query.code;
+        request('https://qyapi.weixin.qq.com/cgi-bin/user/getuserinfo?access_token='+getToken().access_token+'&code=' + code, function(err, ress, data){
+            if(!JSON.parse(data).errcode){
+                req.session.user = JSON.parse(data); 
+                console.log(req.session.user);
+                req.session.user.UserId = '12154545'; 
+                getTeacherInfo(req.session.user.UserId, 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', res);
+            }   
+            else{
+            }
+        }); 
+       
+        }
+
+   });
+
 
 app.listen(3000,function(req,res){
        console.log("app is running at port 3000");
